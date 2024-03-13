d82b848a9cec2b4af8a2743c450a7f52a0ebe113
Chris Phillipson <cphillipson@pivotal.io>
2024-03-12 21:26:06 -0700

    Add script to switch application's backend to MySQL

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ The home for ideas and exploration.  Mainly documentation, videos and discussion
   * [Expose curated set of Spring Boot Actuator endpoints](scripts/expose-actuator-endpoints.sh)
   * Set Java artifacts [fetch mode](scripts/set-java-artifacts-fetch-mode.sh) on a [cf-butler](https://github.com/cf-toolsuite/cf-butler/blob/main/docs/ENDPOINTS.md#java-applications) instance
   * [Update Spring Cloud Config Server instance mirror](scripts/update-config-service-mirrors.sh) backing [cf-hoover](https://github.com/cf-toolsuite/cf-hoover?tab=readme-ov-file#minimum-required-keys) instance
+  * [Switch](scripts/switch-backend-to-mysql.sh) backend to MySQL
---
+++ scripts/switch-backend-to-mysql.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+set -e
+
+# This script:
+#   stops the application
+#   creates a MySQL backend in the target foundation organization and space
+#   calls cf bind-service to bind it to a target application
+#   rebuilds the application w/ the necessary provider dependency
+#   pushes the application to the target foundation organization and space
+
+# You should have access to the source of the application
+# You should be aware of the cf marketplace service offerings to choose the correct/available service plan
+
+ORG_HOME=observability
+SPACE_HOME=cf-toolsuite
+
+if [ -z "$1" ] && [ -z "$2" ]; then
+	echo "Usage: switch-backend-to-mysql.sh {cf-api} {cf-admin-username} {cf-admin-password} {application-name} {application-version} {mysql-service-plan} {path/to/application/source}"
+	exit 1
+fi
+
+# e.g.,
+# Default target, Dhaka:  ./scripts/switch-backend-to-mysql.sh api.sys.dhaka.cf-app.com sso ' '
+# Alternate foundation:  ./scripts/switch-backend-to-mysql.sh api.sys.orca-916168.cf-app.com username 'password'
+
+CF_API="${1}"
+CF_ADMIN="${2}"
+CF_PASSWORD="${3}"
+APP_NAME="${4:-cf-butler}"
+APP_VERSION="${5:-1.0-SNAPSHOT}"
+MYSQL_SERVICE_PLAN="${6:-db-medium-80}"
+PATH_TO_SRC="${7:-/tmp/$APP_NAME}"
+
+
+echo "-- Setting API endpoint for target foundation"
+
+cf api $CF_API
+
+echo "-- Authenticating"
+
+if [ "$CF_ADMIN" = "sso" ]; then
+  cf login --sso
+else
+  cf login -u $CF_ADMIN -p $CF_PASSWORD
+fi
+
+echo "-- Targeting organization and space"
+
+cf target -o $ORG_HOME -s $SPACE_HOME
+
+echo "-- Stopping $APP_NAME"
+
+cf stop $APP_NAME
+
+echo "-- Creating a MySQL database instance"
+
+cf create-service p.mysql $MYSQL_SERVICE_PLAN $APP_NAME-backend
+while [[ $(cf service $APP_NAME-backend) != *"succeeded"* ]]; do
+    echo "$APP_NAME-backend is not ready yet..."
+    sleep 5s
+done
+
+echo "-- Binding $APP_NAME to $APP_NAME-backend"
+
+cf bind-service $APP_NAME $APP_NAME-backend
+
+echo "-- Rebuilding source with correct provider dependency"
+
+cd $PATH_TO_SRC
+
+./mvnw clean package -Pmysql,expose-runtime-metadata
+
+echo "-- Pushing $APP_NAME"
+
+cf push -p target/$APP_NAME-mysql-$APP_VERSION.jar
